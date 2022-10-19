ddfbc4ddf1c8b2f64eb7f31527f95ad7bd6388fa
kim suna <108721070+ssunakimm@users.noreply.github.com>
2022-10-19 18:55:24 +0900

    1019 업로드
    타입스크립트

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var userName = "kim";
+var userAge = 31;
+var userAddr = 'seoul';
+var favorite = {
+    title: '에잇',
+    singer: '아이유'
+};
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true
+};
+var 회원들 = [1, '2', 3];
+var 오브젝트 = { a: '123' };
