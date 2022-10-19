05a5322317a90405cc92d52f346f4527bf94057b
kim suna <108721070+ssunakimm@users.noreply.github.com>
2022-10-19 19:03:57 +0900

    1019 업로드

--- js/index.js
+++ js/index.js
@@ -7,8 +7,20 @@ var favorite = {
 };
 var project = {
     member: ['kim', 'park'],
-    days: 30,
+    days: 31,
     started: true
 };
 var 회원들 = [1, '2', 3];
 var 오브젝트 = { a: '123' };
+//배열 타입지정
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'park',
+    friend: 'john'
+};
+학교.score[4] = false;
+학교.friend = ['lee', 학교.teacher];
