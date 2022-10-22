383e58f21a3741c24d84861b86f09eaebf4b5547
kim suna <108721070+ssunakimm@users.noreply.github.com>
2022-10-22 23:53:03 +0900

    1022 업로드

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './App.css';
+import data from './data.js'
 
 function App() {
+  let [item] = useState(data)
   return (
     <div className="wrapper">
       <header className='header'>
@@ -34,13 +37,36 @@ function App() {
         </nav>
       </header>
       <div className="content">
-
+        <ul>
+          {
+            item.map((item,i)=>{
+              return <Item item={item} key={item.id}></Item>
+            })    
+          }
+          
+        </ul>
       </div>
       <footer className='footer'>
 
       </footer>
     </div>
   );
+}
+
+function Item(props){
+  return (
+    <li>
+      <a href="aa">
+        <img src="aa" />
+        <b>{props.item.title}</b>
+      </a>
+      <ul>
+        <li className='beforePrice'>{props.item.beforePrice}</li>
+        <li className='currentPrice'>{props.item.currentPrice}</li>
+      </ul>
+      
+    </li>
+  )
 }
 
 export default App;
--- react/shop/src/data.js
+++ react/shop/src/data.js
@@ -1,23 +1,26 @@
-[
+let data = [
     {
       id : 0,
       title : "White and Black",
       content : "Born in France",
-      price : 120000
+      beforePrice : 260000,
+      currentPrice : 120000
     },
   
     {
       id : 1,
       title : "Red Knit",
       content : "Born in Seoul",
-      price : 110000
+      beforePrice : 260000,
+      currentPrice : 110000
     },
   
     {
       id : 2,
       title : "Grey Yordan",
       content : "Born in the States",
-      price : 130000
+      beforePrice : 260000,
+      currentPrice : 130000
     }
   ] 
 
