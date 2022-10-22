c22d7ebee1a22c0ebd2f55cba1c8f5f66be86df4
kim suna <108721070+ssunakimm@users.noreply.github.com>
2022-10-22 23:35:53 +0900

    1022 업로드

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className='header'>
+        <nav>
+          <ul>
+            <li>
+              <a href="dd">About</a>
+              <ul>
+                <li><a href="dd">안뇽</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="dd">About</a>
+              <ul>
+                <li><a href="dd">안뇽</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="dd">About</a>
+              <ul>
+                <li><a href="dd">안뇽</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="dd">About</a>
+              <ul>
+                <li><a href="dd">안뇽</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <div className="content">
+
+      </div>
+      <footer className='footer'>
+
+      </footer>
     </div>
   );
 }
